ec49837727ae11b1268e0c0ab5222656b1913c5e
Pascal Pfiffner <phase.of.matter@gmail.com>
2014-12-11 16:33:20 +0100

    Simplify OAuth2 instantiation

--- OAuth2App/AppDelegate.swift
+++ OAuth2App/AppDelegate.swift
@@ -12,7 +12,14 @@ import OAuth2
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate
 {
-	var oauth2: OAuth2?
+	lazy var oauth2 = OAuth2CodeGrant(settings: [
+		"client_id": "8ae913c685556e73a16f",                         // yes, this client-id and secret will work!
+		"client_secret": "60d81efcc5293fd1d096854f4eee0764edb2da5d",
+		"authorize_uri": "https://github.com/login/oauth/authorize",
+		"token_uri": "https://github.com/login/oauth/access_token",
+		"scope": "user repo:status",
+		"redirect_uris": ["ppoauthapp://oauth/callback"],            // don't forget to register this scheme
+	])
 	
 	func applicationWillFinishLaunching(notification: NSNotification) {
 		// register our app to get notified when launched via URL
@@ -24,36 +31,13 @@ class AppDelegate: NSObject, NSApplicationDelegate
 		)
 	}
 	
-	func applicationDidFinishLaunching(aNotification: NSNotification) {
-		let settings = [
-			"client_id": "8ae913c685556e73a16f",
-			"client_secret": "60d81efcc5293fd1d096854f4eee0764edb2da5d",
-			"authorize_uri": "https://github.com/login/oauth/authorize",
-			"token_uri": "https://github.com/login/oauth/access_token",
-			"scope": "user repo:status",
-			"redirect_uris": ["ppoauthapp://oauth/callback"],				// don't forget to register this scheme
-		]
-		
-		// init OAuth2 handle
-		let oauth = OAuth2CodeGrant(settings: settings)
-		oauth.onAuthorize = { parameters in
-			println("Did authorize with parameters: \(parameters)")
-		}
-		oauth.onFailure = { error in        // `error` is nil on cancel
-			if nil != error {
-				println("Authorization went wrong: \(error!.localizedDescription)")
-			}
-		}
-		oauth2 = oauth
-	}
-	
 	
 	// MARK: - OAuth2
 	
 	func requestToken(callback: (wasFailure: Bool, error: NSError?) -> Void) {
-		oauth2!.afterAuthorizeOrFailure = callback
+		oauth2.afterAuthorizeOrFailure = callback
 		
-		let url = oauth2!.authorizeURL()
+		let url = oauth2.authorizeURL()
 		NSWorkspace.sharedWorkspace().openURL(url)
 	}
 	
@@ -62,7 +46,7 @@ class AppDelegate: NSObject, NSApplicationDelegate
 		if let urlString = event.paramDescriptorForKeyword(AEKeyword(keyDirectObject))?.stringValue {
 			let url = NSURL(string: urlString)
 			if "ppoauthapp" == url?.scheme && "oauth" == url?.host {
-				oauth2!.handleRedirectURL(url!)
+				oauth2.handleRedirectURL(url!)
 			}
 		}
 		else {
